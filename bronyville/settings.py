0daa912763f6e4e4e641a230801e150f9fc9819a
KRazit <timurnyaa@gmail.com>
2022-10-19 15:24:11 +0300

    Fix a stupid bug :D

--- bronyville/settings.py
+++ bronyville/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@1*@y91^c_-*f47-j18q9-x57-*2^yg8snun1_*oo0y4h(s-%s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'],
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
